2c1d50aefd46fd6f9ed7407b733b53adf25b1b14
Cristhina Urra Aldunate <40528744+cristianurra@users.noreply.github.com>
2025-10-31 12:24:01 -0300

    Fix print statements and enhance video processing

--- detector.py
+++ detector.py
@@ -1,11 +1,13 @@
 import cv2
 import numpy as np
+
 nombre_video_entrada = 'output_derecha.mp4'
 nombre_video_salida = 'salida.mp4'
+
 cap = cv2.VideoCapture(nombre_video_entrada)
 
 if not cap.isOpened():
-    prin(f"ERROR al abrir")
+    print(f"ERROR al abrir {nombre_video_entrada}")
     exit()
 
 fps = cap.get(cv2.CAP_PROP_FPS)
@@ -16,10 +18,11 @@ fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter(nombre_video_salida, fourcc, fps, (width, height), isColor=True)
 
 if not out.isOpened():
-    print("no se puede guardar")
+    print("No se puede guardar el video de salida")
     cap.release()
     exit()
-
+kernel_unir = np.ones((5, 5), np.uint8) 
+kernel_limpiar = np.ones((3, 3), np.uint8) 
 while True:
     ret, frame = cap.read()
     if not ret:
@@ -34,9 +37,11 @@ while True:
         blockSize=15,
         C=1
     )
-    cuerdas_bgr = cv2.cvtColor(cuerdas_binario, cv2.COLOR_GRAY2BGR)
-    #out.write(cuerdas_bgr)
-    cv2.imshow('Cuerdas Adaptativas (Presiona q para salir)', cuerdas_binario)
+    cuerdas_cerradas = cv2.morphologyEx(cuerdas_binario, cv2.MORPH_CLOSE, kernel_unir)
+    cuerdas_filtradas = cv2.morphologyEx(cuerdas_cerradas, cv2.MORPH_OPEN, kernel_limpiar)
+    cuerdas_bgr = cv2.cvtColor(cuerdas_filtradas, cv2.COLOR_GRAY2BGR)
+    out.write(cuerdas_bgr) 
+    cv2.imshow('Cuerdas Unificadas y Filtradas (Presiona q para salir)', cuerdas_filtradas) 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cap.release()
